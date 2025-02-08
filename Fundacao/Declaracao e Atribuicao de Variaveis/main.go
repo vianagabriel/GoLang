f4de31dae06878148527cf082d114710529ea539
Gabriel Viana <vianagabrieldacunha@gmail.com>
2025-02-08 01:00:15 -0300

    fonte que explica sobre declaração e att de variáveis

---
+++ Fundacao/Declaracao e Atribuicao de Variaveis/main.go
@@ -0,0 +1,27 @@
+package main
+
+// Tipos de dados
+
+// Escopo global
+var (
+	exist        bool    //   Boleano
+	num          int     //   Inteiro
+	texto        string  //   texto
+	numFlutuante float64 //   numero flutuante
+)
+
+func main() {
+	exist = true
+	num = 10
+	texto = "Tipo Texto"
+	numFlutuante = 10.2
+
+	local := "Variável escopo local"
+
+	println(exist)
+	println(num)
+	println(texto)
+	println(numFlutuante)
+	println(local)
+
+}
