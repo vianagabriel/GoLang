796205ffbaba189679b8298a4fa2a8f57453c9cc
Gabriel Viana <vianagabrieldacunha@gmail.com>
2025-02-22 18:04:14 -0300

    funções variadicas

--- Fundacao/Funcoes_Variadicas/main.go
+++ Fundacao/Funcoes_Variadicas/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+// Função variádica que soma vários números
+func soma(numeros ...int) int {
+    total := 0
+    for _, num := range numeros {
+        total += num
+    }
+    return total
+}
+
+func main() {
+    fmt.Println(soma(1, 2, 3))       // Saída: 6
+    fmt.Println(soma(10, 20, 30, 40)) // Saída: 100
 }
